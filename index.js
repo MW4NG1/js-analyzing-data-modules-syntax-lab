38baa5a946853b857f6c8df076b85857ace5125c
MW4NG1 <msage3803@gmail.com>
2026-06-29 17:12:28 +0300

    Implement combineUsers function and update package name in package-lock.json

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+// Create a function to combine users
+function combineUsers(...args) {
 
+  // Create an object that will hold the combined users
+  const combinedObject = {
+  users: []  //Create array to hold users and initialize with empty array 
+ };
+
+//  Loop through args(argument) one array at a time
+for (const array of args){
+
+}
+
+};
 
 
 module.exports = {
