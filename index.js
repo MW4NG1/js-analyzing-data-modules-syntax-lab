b017a2d656429b7129e62d5de2cee8fce5e895ac
MW4NG1 <msage3803@gmail.com>
2026-06-29 17:31:52 +0300

    Refactor combineUsers function for improved readability and consistency

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
+// Import the datejs library
+require("datejs");
+
 // Create a function to combine users
 function combineUsers(...args) {
-
   // Create an object that will hold the combined users
   const combinedObject = {
-  users: []  //Create array to hold users and initialize with empty array 
- };
+    users: [], //Create array to hold users and initialize with empty array
+  };
 
-//  Loop through args(argument) one array at a time
-for (const array of args){
+  //  Loop through args(argument) one array at a time
+  for (const array of args) {
 
+    // Merge the users from current array into users
+    combinedObject.users = [
+      ...combinedObject.users, //Spread/Keep the exisitng usernames
+      ...array //Add usernames from the current array
+    ];
+  }
+  // Add today's date in the format M/D/YYYY
+  combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+  // Return the combined object
+  return combinedObject;
 }
 
-};
-
-
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
